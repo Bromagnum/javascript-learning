b938fcb95ff76260c12d78570bd2bfc26fb19773
Göktuğ Özfen <goktugozfen@gmail.com>
2025-11-02 18:08:29 +0300

    .

---
+++ app.js
@@ -0,0 +1,112 @@
+// Let  Keyword
+
+// let userEmail = "iman@gmail.com";
+// let age = 29;
+
+// age = age + 1;
+
+// const keyword
+
+// const points = 100;
+
+// Currency Converter
+//$AUD tı $USD AUD = USD * 1.5
+
+// let USD = 1200;
+// let AUD = USD * 1.5;
+// console.log(AUD);
+
+// let comparison = "4" != 4
+// console.log(comparison)
+//Conditionals
+
+// let hasMemberShip = false;
+// let signedIn = true;
+
+// if (hasMemberShip === true) {
+//   console.log("Show user the video");
+// } else if (signedIn === true) {
+//   console.log("get an upgrade bitch");
+// } else {
+//   console.log("Fuck you");
+// }
+
+// let age = 19;
+
+// if (age > 18) {
+//   console.log(`you are ${age} years old you can enter the club`);
+// } else if (age === 18) {
+//   console.log("you are 18 welcome bitch");
+// }else{
+//     console.log(`you are ${age} years old grow up than come back idiot`)
+// }
+
+// let age = 18;
+// let hasId = false;
+
+// if (age >= 18 && !hasId ) {
+//   console.log("You may enter the fuckn club");
+// }
+
+// Falsey Values
+
+// let age = 18;
+// let hasId = false;
+// if(age){
+// console.log("you may enter the club")
+// }
+// Ternary Operator
+
+// let isRaining = true;
+// isRaining ? console.log("it is raining") : console.log("its not raining");
+
+// let age = 19;
+// let hasId = true;
+
+// let str = age >= 18 && hasId ? "You may enter the club" : "Fuck off";
+// console.log(str);
+
+// Example :
+// let loggedIn = true;
+// let hasMemberShip = true;
+
+// let str = loggedIn && hasMemberShip ? "show the video" : "dont show the video";
+
+// console.log(str);
+//Loops
+
+// DRY => Dont Repeat Yourself
+// let counter = 1;
+// while (counter <= 10) {
+//   console.log(counter);
+//   counter = counter + 1;
+// }
+
+// console.log("While loop finishe running");
+
+// for (let i = 1; i <= 50; i++) {
+//     console.log(i);
+// }
+// Exampe : for-loop 1,15
+//if number is odd print ASAp
+//if number is even print Frontend
+//if divisible by 5 print ASAP Frontend
+
+// for (let i = 1; i <= 15; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i + " ASAP Frontend");
+//   } else if (i % 2 === 0) {
+//     console.log(i + " Frontend");
+//   } else if (i % 2 === 1) {
+//     console.log(i + " ASAP");
+//   }
+// }
+
+// Example: print every character from string "ASAP Frontend"
+
+// let str = "ASAP Frontend"
+
+// for(let i = 0;i <= str.length; i++){
+
+// console.log(str[i])
+// }
---
+++ functions.js
@@ -0,0 +1,36 @@
+// fonction decleration
+
+// function greetUser(user, time) {
+//   console.log(`Good ${time} ${user}`);
+// }
+// greetUser("Göktuğ","morning");
+// greetUser("Recep","evening");
+// greetUser("Mahmut","afternoon");
+
+// function main(){
+//     return "ASAP Frontend"
+
+// }
+// console.log(main());
+
+// function sum(num1,num2){
+//     return num1 + num2
+// }
+// console.log(sum(6,7))
+
+// Example : $AUD to $USD AUD = USD * 1.5
+
+// function convertToAud(usd) {
+//   let aud = usd * 1.5;
+//   return `${aud}$`;
+// }
+// console.log(convertToAud(1000));
+// console.log(convertToAud(500));
+// console.log(convertToAud(200));
+
+//Arrow Function
+
+// const convertToAud = (dollars) => {
+//   return dollars * 1.5;
+// };
+// console.log(convertToAud(100));
